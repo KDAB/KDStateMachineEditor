KDAB/KDStateMachineEditor

commit 32ba3d8d1d8cc9aa56efb32c3319b1ba747e3c79

FOCA-137: Fix adjusting of state labels
Prior to this patch, a change of the state label could cause the visual
representation of this element to loose its initial geometry. This patch
now no longer recalculates the geometry of a state layout item when the
label got changed. Leave that to the layouter if needed.

Initially, that code was there to be able to get a somewhat useful
visual appearance of the state chart when no layouter was present.

Change-Id: Idf3d2bdb961b021220905d69285fdbddcd17b337

src/core/layout/layoutitem.cpp
@@ -248,51 +248,6 @@ QList<TransitionLayoutItem*> StateLayoutItem::transitions() const
     return ObjectHelper::copy_if_type<TransitionLayoutItem*>(children());
 }
 
-void StateLayoutItem::elementChanged(Element* oldElement, Element* newElement)
-{
-    KDSME::LayoutItem::elementChanged(oldElement, newElement);
-
-    if (oldElement) {
-        disconnect(oldElement, &Element::labelChanged, this, &StateLayoutItem::updateItem);
-    }
-    if (newElement) {
-        connect(newElement, &Element::labelChanged, this, &StateLayoutItem::updateItem);
-    }
-    updateItem();
-}
-
-void StateLayoutItem::updateItem()
-{
-    if (!element())
-        return;
-
-    const QSizeF oldSize = QSizeF(height(), width());
-
-    QSizeF newSize;
-    switch (element()->type())
-    {
-    case Element::StateMachineType:
-        newSize = boundingRect().size().expandedTo(LayoutUtils::sizeForLabel(element()->label()));
-        break;
-    case Element::FinalStateType:
-        newSize = QSizeF(32, 32);
-        break;
-    case Element::HistoryStateType:
-    case Element::StateType:
-        newSize = LayoutUtils::sizeForLabel(element()->label());
-        break;
-    default:
-        break;
-    }
-
-    if (oldSize == newSize)
-        return;
-
-    prepareGeometryChange();
-    setWidth(newSize.width());
-    setHeight(newSize.height());
-}
-
 TransitionLayoutItem::TransitionLayoutItem(StateLayoutItem* parent)
     : LayoutItem(parent)
     , m_targetState(nullptr)

src/core/layout/layoutitem.h
@@ -149,12 +149,6 @@ public:
 Q_SIGNALS:
     void expandedChanged(bool expanded);
 
-protected:
-    virtual void elementChanged(Element* oldElement, Element* newElement);
-
-private Q_SLOTS:
-    void updateItem();
-
 private:
     bool m_expanded;
 };

commit 472fc6215ec1b029871efe46c529d40e222e5578

Fix drag and drop state
The regression was introduced with commit 32ba3d8d1d8

In case of drag and drop the newley created LayoutItem
needs a proper initial size. The commit above removed that
and we ended with a zero size. An alternate, more
offensive, way could be to just relayout.

Change-Id: Ib165c0ac00f53c69a14929e500a158fba066a8f9
Reviewed-on: https://codereview.kdab.com/13447
Reviewed-by: Continuous Integration <build@kdab.com>
Reviewed-by: Volker Krause <volker@kdab.com>

src/core/layout/layoutitem.cpp
@@ -151,6 +151,33 @@ void LayoutItem::setSelected(bool selected)
     emit selectedChanged(d->m_selected);
 }
 
+QSizeF LayoutItem::preferredSize() const
+{
+    if (!element())
+        return QSizeF();
+    QSizeF size;
+    switch (element()->type())
+    {
+    case Element::HistoryStateType:
+    case Element::StateType:
+        size = LayoutUtils::sizeForLabel(element()->label());
+        break;
+    case Element::StateMachineType:
+        size = boundingRect().size().expandedTo(LayoutUtils::sizeForLabel(element()->label()));
+        break;
+    case Element::FinalStateType:
+        size = QSizeF(32, 32);
+        break;
+    case Element::ElementType:
+    case Element::TransitionType:
+    case Element::SignalTransitionType:
+    case Element::TimeoutTransitionType:
+    case Element::PseudoStateType:
+        break;
+    }
+    return size;
+}
+
 QRectF LayoutItem::boundingRect() const
 {
     const QRectF rect(pos().x(), pos().y(), width(), height());

src/core/layout/layoutitem.h
@@ -87,6 +87,7 @@ public:
     bool isSelected() const;
     void setSelected(bool selected);
 
+    QSizeF preferredSize() const;
     virtual QRectF boundingRect() const;
 
     View* view() const;

src/view/widgets/statemachineview.cpp
@@ -352,12 +352,17 @@ bool StateMachineView::sendDropEvent(LayoutItem* sender, LayoutItem* target, con
             Q_ASSERT(m_view->rootLayoutItem());
             LayoutImportExport::importLayout(doc.object(), m_view->rootLayoutItem());
 
-            // Now move the new element to its position
+            // move the new element to its position and set a sane initial size
             LayoutItem* layoutitem = m_view->layoutItemForElement(element);
             Q_ASSERT(layoutitem);
             ModifyLayoutItemCommand poscmd(layoutitem);
-            QPointF p = m_pos - layoutitem->pos();
-            poscmd.moveBy(p.x(), p.y());
+            QPointF pos = m_pos;
+            QSizeF size = layoutitem->preferredSize();
+            if (size.width() > 0)
+                pos.setX(qMax<qreal>(0, pos.x() - size.width()/2));
+            if (size.height() > 0)
+                pos.setY(qMax<qreal>(0, pos.y() - size.height()/2));
+            poscmd.setGeometry(QRectF(pos, size));
             poscmd.redo();
 
             // TODO this rearranges the just created and positioned element, why?