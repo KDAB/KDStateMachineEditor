8b03afd402f82f2ca9cdfecb665b784d3eda2e09
Allen Winter <allen.winter@kdab.com>
2024-04-04 06:49:02 -0400

    sync to upstream KDAB and KDE cmake files

--- cmake/ECM/modules/ECMQueryQmake.cmake
+++ cmake/ECM/modules/ECMQueryQmake.cmake
@@ -1,3 +1,7 @@
+if (${ECM_GLOBAL_FIND_VERSION} VERSION_GREATER_EQUAL 5.93)
+    message(DEPRECATION "ECMQueryQmake.cmake is deprecated since 5.93, please use ECMQueryQt.cmake instead.")
+endif()
+
 include(${CMAKE_CURRENT_LIST_DIR}/QtVersionOption.cmake)
 find_package(Qt${QT_MAJOR_VERSION}Core QUIET)
 
